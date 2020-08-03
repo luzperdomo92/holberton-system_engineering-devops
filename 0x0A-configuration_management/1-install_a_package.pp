5d94b85920920492475a950df8a7f46b4027f9fe
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-03 15:20:03 -0500

    added provider: gem

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # install puppet-lint using puppet
 package { 'puppet-lint':
-  ensure   => 'created'
+  ensure   => 'created',
+  provider => 'gem',
 }
 exec { 'puppet-lint':
     command => 'gem install puppet-lint -v 2.1.1'
