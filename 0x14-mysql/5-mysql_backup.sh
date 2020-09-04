4c7988d032d20011679e3444f10dee219aa219fe
Luz Perdomo <lilyperdomo92@gmail.com>
2020-09-03 21:22:23 -0500

    fixed syntax

--- 0x14-mysql/5-mysql_backup.sh
+++ 0x14-mysql/5-mysql_backup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# generates a MySQL dump, creates a compressed archive out of it.
+# generates a MySQL dump and creates a compressed archive out of it
 
-mysqldump -u root -p"$1" --all-databases > backup.sql
+mysqldump -uroot -p"$1" --all-databases > backup.sql
 tar -czf $(date +%d-%m-%Y).tar.gz backup.sql
