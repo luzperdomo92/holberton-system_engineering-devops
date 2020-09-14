7b6755f755a0f8987275eb21e810de7040cc0586
Luz Perdomo <lilyperdomo92@gmail.com>
2020-09-14 16:36:58 -0500

    suing get method for dicts

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """ Acceding and return informacion using API """
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
     """  """
 
-id = sys.argv[1]
-res_user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(id))
-res_todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                        .format(id))
-users = res_user.json()
-todos = res_todo.json()
+user_id = argv[1]
+res_user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(user_id))
+res_todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(user_id))
+user = res_user.json()
+todo = res_todo.json()
 
 tasks = []
-for task in todos:
-    if task["completed"] is True:
-        tasks.append(task.get("title")
-print("Employee {} is done with tasks({}/{}):".format(users.get("name"),
-      len(tasks), len(todos)))
+for task in todo:
+    if task.get("completed") is True:
+        tasks.append(task.get("title"))
+print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
+      len(tasks), len(todo)))
 
 for task in tasks:
     print("\t {}".format(task))
