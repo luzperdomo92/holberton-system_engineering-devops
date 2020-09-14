af7a7c8dc8524a0d4aa92bd31f9770ea5b4cc52a
Luz Perdomo <lilyperdomo92@gmail.com>
2020-09-14 16:08:32 -0500

    fixed spaces between parentesis

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     """  """
 
-id = int(sys.argv[1])
+id = sys.argv[1]
 res_user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(id))
 res_todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
@@ -19,7 +19,7 @@ tasks = []
 for task in todos:
     if task["completed"] is True:
         tasks.append(task["title"])
-print("Employee {} is done with tasks ({}/{}):".format(users["name"],
+print("Employee {} is done with tasks({}/{}):".format(users["name"],
       len(tasks), len(todos)))
 
 for task in tasks:
