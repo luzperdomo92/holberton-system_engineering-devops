97b1b1d29f2de46a49a1e0471a8228463bbf0dea
Luz Perdomo <lilyperdomo92@gmail.com>
2020-09-14 16:40:49 -0500

    fixed tab in main function

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,20 +7,20 @@ from sys import argv
 if __name__ == "__main__":
     """  """
 
-user_id = argv[1]
-res_user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(user_id))
-res_todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(user_id))
-user = res_user.json()
-todo = res_todo.json()
+    user_id = argv[1]
+    res_user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                            format(user_id))
+    res_todo = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id))
+    user = res_user.json()
+    todo = res_todo.json()
 
-tasks = []
-for task in todo:
-    if task.get("completed") is True:
-        tasks.append(task.get("title"))
-print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
-      len(tasks), len(todo)))
+    tasks = []
+    for task in todo:
+        if task.get("completed") is True:
+            tasks.append(task.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
+          len(tasks), len(todo)))
 
-for task in tasks:
-    print("\t {}".format(task))
+    for task in tasks:
+        print("\t {}".format(task))
