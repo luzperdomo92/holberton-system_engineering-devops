9d2d288cf47a27a80db3e64c65518583585a163c
Luz Perdomo <lilyperdomo92@gmail.com>
2020-09-17 18:39:18 -0500

    added second commit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,20 +2,24 @@
 """ Acceding and return informacion using API """
 import requests
 
-def recurse(subreddit, hot_list=[]):
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+def recurse(subreddit, hot_list=[], after=""):
+    """ """
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(
+        subreddit, after)
     headers = {"User-Agent": "luzperdomo"}
     response = requests.get(url, headers=headers)
-    json_request = response.json()
-    hot_posts = json_request.get('data', {}).get('children', None)
 
-    if response.status_code != 200:
-        return None
+    if response:
+        data = response.json().get('data')
+        children = data.get('children')
+        after = data.get('after')
+        for item in children:
+            hot_list.append(item.get('data').get('title'))
 
-    if hot_list == []:
-        return None
+        if after is not None and children:
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
     else:
-        searh_data = hot_list[0]
-        list_title = hot_list[1:]
-        return searh_data + recurse(list_title)
+        return None
