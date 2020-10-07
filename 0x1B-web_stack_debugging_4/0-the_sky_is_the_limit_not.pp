716757f576540b3e66330e6eb1090a24fb2fabed
Luz Perdomo <lilyperdomo92@gmail.com>
2020-10-07 18:07:21 -0500

    first commit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+exec { '/etc/default/nginx':
+  command  => "echo 'ULIMIT=\"-n 4096\"' > /etc/default/nginx",
+  provider => 'shell'
+}
+-> exec { 'Restart Nginx':
+  command  => 'sudo service nginx restart',
+  provider => 'shell'
+}
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+exec { '/etc/default/nginx':
+  command  => "sed -i 's/holberton hard nofile 5/holberton hard nofile 1000/' /etc/security/limits.conf",
+  provider => 'shell'
+}
+-> exec { '/etc/security/limits.conf':
+  command  => "sed -i 's/holberton soft nofile 4/holberton soft nofile 1000/' /etc/security/limits.conf",
+  provider => 'shell'
+}
+-> exec { 'Restart command':
+  command  => 'sysctl -p',
+  provider => 'shell'
+}
